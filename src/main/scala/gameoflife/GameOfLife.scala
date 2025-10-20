129c2922a4647d709b346c1fe8a3dc11a16f965b
Brian Jing <brian.jing.dev@gmail.com>
2025-10-20 21:39:31 +1100

    Refactor and comments

--- src/main/scala/gameoflife/GameOfLife.scala
+++ src/main/scala/gameoflife/GameOfLife.scala
@@ -5,7 +5,7 @@ import cats.implicits._
 import cats.syntax.all._  // for traverse_
 
 type Cell = (Int, Int)
-// TODO no need for map because boolean value is redundant
+// TODO no need for map because the boolean value are redundant
 // However, the constant-time access of a Map is efficient for updating states
 // Think over it once tests are done
 type Matrix = Map[Cell, Boolean]
@@ -13,34 +13,47 @@ val MatrixSize = 200 // 200 x 200
 val Generations = 100 //
 
 object GameOfLife {
-  def runGame(matrix: Matrix, matrixSize: Int, generations: Int): IO[Unit] = {
-    val result: List[Set[Cell]] = (1 to generations).toList.flatMap{ gen =>
-      val newState = nextGeneration(matrix, matrixSize)
-      if newState.keySet.size.isEmpty then None
-      else Some(newState.keySet)
-    }
-
-    if result.nonEmpty then
-      result.zip(1 to result.length).traverse_ (
-        (state, index) => {
-          val formattedState = state
-            .map { case (x, y) => s"[$x, $y]" }
-            .mkString("[", ", ", "]")
-          IO.println(s"$index: $formattedState")
+  def runGame(matrix: Matrix, matrixSize: Int, generations: Int): IO[Unit] =
+    for {
+      matrixStates <- IO.pure {
+        // TODO bug found, we are not iterating on the previous state
+        (1 to generations).toList.flatMap { gen =>
+          val newState = nextGeneration(matrix, matrixSize)
+          if newState.keySet.size.isEmpty then None
+          else Some(newState.keySet)
         }
-      )
-    else
-      IO.println("[]")
-  }
+      }
+      _ <- printMatrixStates(matrixStates)
+    } yield ()
 
+  /*
+    Populate matrix Map with a list of live cell data
+  */
   def initMatrix(liveCells: List[Cell]): Matrix = {
     liveCells.map {
       case (x, y) => ((x, y),  true)
     }.toMap
   }
 
-  // Generate next state for matrix
-  // Only store live cells coordinates in Map
+  def printMatrixStates(matrixStates: List[Set[Cell]]): IO[Unit] =
+    if matrixStates.nonEmpty then
+      matrixStates
+        .zip(1 to matrixStates.length)
+        .traverse_ (
+          (state, index) => {
+            val formattedState = state
+              .map { case (x, y) => s"[$x, $y]" }
+              .mkString("[", ", ", "]")
+            IO.println(s"$index: $formattedState")
+          }
+        )
+    else
+      IO.println("[]")
+
+  /*
+    Generate next state for matrix
+    Only store live cells coordinates in Map
+   */
   def nextGeneration(matrix: Matrix, matrixSize: Int): Matrix = {
     val newState =
       (for {
@@ -55,7 +68,9 @@ object GameOfLife {
     newState.toMap
   }
 
-  // Calculate next state for given cell coordinates
+  /*
+    Calculate next state for given cell coordinates
+  */
   def nextStateForCell(cell: Cell, matrix: Matrix): Boolean = {
     val neighbourStates = cell match {
       case (x, y) =>
